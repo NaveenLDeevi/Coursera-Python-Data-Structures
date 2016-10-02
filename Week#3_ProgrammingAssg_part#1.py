dc9ccaef1722dec4f264907e9c287cc3282657da
NAVEEN <naveenlokanadhd@gmail.com>
2016-10-01 22:43:42 -0500

    Coursera-Data Structures Week3 Programming assiginment 1

---
+++ Week#3_ProgrammingAssg_part#1.py
@@ -0,0 +1,11 @@
+# Coursera- Data structures course - Week#3.
+#7.1 Write a program that prompts for a file name, then opens that file and reads 
+#through the file, and print the contents of the file in upper case. Use the file 
+#words.txt to produce the output below.You can download the sample data at 
+#http://www.pythonlearn.com/code/words.txt
+#!/usr/bin/env python
+
+input = raw_input('Enter the file name:')
+int = open(input,'r')
+for line in int:
+	print (line.upper()).strip()
